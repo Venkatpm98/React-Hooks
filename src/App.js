0fdaacbad83ab910ae009abfcc5dedafa8a35ff6
Venkatpm98 <78491455+Venkatpm98@users.noreply.github.com>
2021-06-07 18:11:20 +0530

    Hooks Day 1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterfour from './components/HookCounterfour';
+import HookCounterfive from './components/HookCounterfive';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ClassCounter /> */}
+      {/* <HookCounter /> */}
+      {/* <HookCounterTwo /> */}
+      {/* <HookCounterThree/> */}
+      {/* <HookCounterfour/> */}
+      {/* <HookCounterfive/> */}
+      {/* <HookMouse/>  */}
+      <MouseContainer />
+
+    </div>
+  );
+}
+
+export default App;
